b0a86cfe3141431fccd42f88b392d6a8a9ed274f
AlexeyErmakov <lexlaeda30@gmail.com>
2020-06-01 15:21:31 +0300

    add favicon

--- src/main/java/com/tracking/config/ErrorController.java
+++ src/main/java/com/tracking/config/ErrorController.java
@@ -5,10 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-
+@ControllerAdvice
 public class ErrorController {
 
     private static Logger logger = LoggerFactory.getLogger(ErrorController.class);
